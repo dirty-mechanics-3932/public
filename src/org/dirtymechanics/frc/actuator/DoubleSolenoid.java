f08bf0b069e493b47b3a626c256634d791c4b0a4
Adam Gresh <adam.gresh@modernizingmedicine.com>
2014-09-12 23:07:04 -0400

    remove dead code

--- src/org/dirtymechanics/frc/actuator/DoubleSolenoid.java
+++ src/org/dirtymechanics/frc/actuator/DoubleSolenoid.java
@@ -2,48 +2,11 @@ package org.dirtymechanics.frc.actuator;
 
 import edu.wpi.first.wpilibj.Solenoid;
 import org.dirtymechanics.frc.util.Updatable;
-
-/**
- * Controls a double solenoid with logic to debounce input and disable redundant
- * relays to conserve power.
- *
- * @author Daniel Ruess
- */
 public class DoubleSolenoid implements Updatable {
-
-    /**
-     * The time to wait after firing to disable the open valve.
-     */
-    private static final int FIRE_WAIT = 1000;
-
-    /**
-     * The spike controlling the opening of the valve.
-     */
     private final Solenoid openSpike;
-    /**
-     * The spike controlling the closing of the valve.
-     */
     private final Solenoid closeSpike;
-    /**
-     * The state of the solenoid.
-     */
     private boolean state;
-    /**
-     * Whether or not the state has changes.
-     */
-    private boolean stateChanged = true;
-    /**
-     * The time of the last state change.
-     */
-    private long lastStateChange;
 
-    /**
-     * Creates a new double solenoid with a 1000ms debounce and initial state of
-     * false.
-     *
-     * @param openSpike The spike controlling the open valve.
-     * @param closeSpike The spike controlling the close valve.
-     */
     public DoubleSolenoid(Solenoid openSpike, Solenoid closeSpike) {
         this(openSpike, closeSpike, false);
     }
@@ -53,7 +16,7 @@ public class DoubleSolenoid implements Updatable {
      *
      * @param openSpike The spike controlling the open valve.
      * @param closeSpike The spike controlling the close valve.
-     * @param initialState The initial state to start in.
+     * @param initialState 
      */
     public DoubleSolenoid(Solenoid openSpike, Solenoid closeSpike, boolean initialState) {
         this.openSpike = openSpike;
@@ -67,9 +30,6 @@ public class DoubleSolenoid implements Updatable {
      * @param state The state of the solenoid.
      */
     public void set(boolean state) {
-        if (this.state != state) {
-            stateChanged = true;
-        }
         this.state = state;
     }
 
